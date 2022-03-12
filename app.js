ab64cd5bc1e450f7852e4e2a187e8105ad1a585c
ahmed-alhomsi <ahmed.alhomsi.official@gmail.com>
2022-03-12 22:18:33 +0200

    made the portfolio responsive, added call to action functionality.

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.navbar ul');
 const projectsContainer = document.querySelector('.projects-container');
 const cards = document.querySelectorAll('.projects-container .card')
 const nextBtn = document.querySelector('#nextBtn');
 const prevBtn = document.querySelector('#prevBtn');
-
 let counter = 1;
 // const size = cards[0].clientWidth;
 const size = 600;
-
 projectsContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+navToggle.addEventListener('click', ()=>{
+    if(navLinks.classList.contains("none")) {
+        navLinks.style.transition = "transform 0.5s ease-in-out";
+        navLinks.classList.remove("none");
+        navLinks.style.display = "block";
+    } else {
+        navLinks.style.transition = "transform 0.5s ease-in-out";
+        navLinks.classList.add("none");
+        navLinks.style.display = "none";
+    }
+});
 
 nextBtn.addEventListener('click', ()=>{
     if(counter >= cards.length - 1) return;
     projectsContainer.style.transition = "transform 0.4s ease-in-out";
     counter++;
     projectsContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-})
+});
 
 prevBtn.addEventListener('click', ()=>{
     if(counter <= 0) return;
@@ -22,7 +34,7 @@ prevBtn.addEventListener('click', ()=>{
     counter--;
     projectsContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-})
+});
 
 projectsContainer.addEventListener('transitionend', ()=>{
     if(cards[counter].id === 'lastClone') {
@@ -34,4 +46,4 @@ projectsContainer.addEventListener('transitionend', ()=>{
         counter = cards.length - counter;
         projectsContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-})
+});
